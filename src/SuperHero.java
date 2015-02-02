4c3dac6be64ec42516e7e29b1b6c4da28e1c1c8d
Brazen Kallio <brazen101@hotmail.ca>
2015-02-02 00:06:16 -0500

    Created new SuperHero class and added the superPowers string array.

---
+++ src/SuperHero.java
@@ -0,0 +1,18 @@
+
+
+public class SuperHero extends Hero {
+
+	public SuperHero(String name) {
+		super(name);
+		String[] superPowers = new String[6];
+		
+		superPowers[0] = "Super Speed";
+		superPowers[1] = "Super Stength";
+		superPowers[2] = "Body Armour";
+		superPowers[3] = "Flight";
+		superPowers[4] = "Fire Generation";
+		superPowers[5] = "Weather Manipulation";
+		
+	}
+
+}
